da4ad7bdcf15e0911f0f0a03fa95a8d0985e03cb
Vivien DE BONA <vdebona@umanit.fr>
2023-01-31 23:36:27 +0100

    feat: start command to update tokens

---
+++ cmd/updateTokens.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2023 DjLeChuck <djlechuck@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+	"github.com/spf13/cobra"
+)
+
+// updateTokensCmd represents the updateTokens command
+var updateTokensCmd = &cobra.Command{
+	Use:   "updateTokens",
+	Short: "Update all tokens",
+	Long: `Launch the update process to compare latest available tokens with the ones in your tokens assets directory.
+
+You will need to give your Patreon session's cookie in order to be able to download the files.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fp := gofeed.NewParser()
+		page := 1
+		hasItems := true
+
+		for ok := true; ok; ok = hasItems {
+			feed, _ := fp.ParseURL(
+				fmt.Sprintf(
+					"https://www.forgotten-adventures.net/product-category/tokens/feed/?paged=%d", page,
+				),
+			)
+
+			if nil == feed {
+				hasItems = false
+			} else {
+				for _, item := range feed.Items {
+					fmt.Println(item.Title)
+				}
+
+				page++
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(updateTokensCmd)
+}
--- go.mod
+++ go.mod
@@ -5,22 +5,30 @@ go 1.18
 require (
 	github.com/antchfx/htmlquery v1.3.0
 	github.com/cavaliergopher/grab/v3 v3.0.1
+	github.com/mmcdole/gofeed v1.2.0
 	github.com/rs/zerolog v1.29.0
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.15.0
 	golang.design/x/clipboard v0.6.3
+	golang.org/x/net v0.5.0
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/antchfx/xpath v1.2.3 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mmcdole/goxpp v0.0.0-20200921145534-2f3784f67354 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
@@ -30,7 +38,6 @@ require (
 	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
 	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
-	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
